74777b7d3cd3a5ad15838f858c65f096f5081bbb
prachinta <pradipchinta8@gmail.com>
2021-10-15 15:45:23 +0530

    simple navbar addtocard created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import Navbar from "./Components /Navbar";
+import ButtonAppBar from "./Components /ButtonAppBar";
+function App() {
+  return (
+    <div>
+      <ButtonAppBar />
+    </div>
+  );
+}
+
+export default App;
